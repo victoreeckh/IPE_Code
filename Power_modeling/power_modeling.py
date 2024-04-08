99595f7ede080443f03663e027f20ed1f4c87f64
Victor <victor.eeckhout@gmail.com>
2024-04-08 09:27:40 +0200

    Optimal solar angle fixed

--- Power_modeling/power_modeling.py
+++ Power_modeling/power_modeling.py
@@ -40,21 +40,30 @@ eta_degrad = 1                            #approximation
 eta_inverter = 0.95
 P_inverter_max = 1e20
 
+#Battery characteristics (SMA Home Storage 3.2)
+P_bat_charge_max = 7500 #W
+P_bat_discharge_max = 6000 #W (From inverter datasheets??)
+E_bat_max = 3.28 #kWh
+eta_bat = 0.945
+
 """# Download data"""
 
 #from single GTI files
 GTI_EW = np.load(project_dir+\
-            f'/effective irradiance /Output_data/total_irradiance_per_angle/total_irradiance_for_tilt_angle_{optimal_east_west_tilt_angle}.npy')
-GTI_E = GTI_EW[0,:]
-GTI_W = GTI_EW[2,:]
+            f'/effective_irradiance/Output_data/total_irradiance_per_angle/total_irradiance_for_tilt_angle_{optimal_east_west_tilt_angle}.npy')
+GTI_E = GTI_EW[0,:].reshape(365,24,60)
+GTI_W = GTI_EW[2,:].reshape(365,24,60)
 
 GTI_S = np.load(project_dir+\
-            f'/effective irradiance /Output_data/total_irradiance_per_angle/total_irradiance_for_tilt_angle_{optimal_south_tilt_angle}.npy')
-GTI_S = GTI_S[1,:]
+            f'/effective_irradiance/Output_data/total_irradiance_per_angle/total_irradiance_for_tilt_angle_{optimal_south_tilt_angle}.npy')
+GTI_S = GTI_S[1,:].reshape(365,24,60)
 
 #Cell temperature data
 T_CommRoof = np.load(project_dir+'/Data_processing/Output_data/processed_T_CommRoof_data.npy')
 T_RV = np.load(project_dir+'/Data_processing/Output_data/processed_T_RV_data.npy')
+
+#load data
+P_load = np.load(project_dir+"Data_processing/Output_data/processed_load_data.npy")
 
 """# Power modeling calculations"""
 
@@ -65,17 +74,30 @@ if Flat_roof:
 else:
     T_cell = T_RV
 
+eta_temp = 1 - beta_ref*(T_cell-T_ref)
+eta_panel = eta_cell * eta_shade * eta_obstruct * eta_temp * eta_degrad
+eta_panel = eta_panel.reshape(365,24,60)
+# print(eta_panel.shape)
+
 if Southern_orientation_case:
+    GTI = GTI_S
+else:
+    GTI = GTI_E      #klopt dit?
 
-    P_sun = GTI_S*n*A                                                   #(365,24,60)
+P_sun = GTI*n*A                                                     #(365,24,60)
+P_panel = P_sun*eta_panel                                           #Element wise product
 
-    if not Battery_case:
+P_inverter = eta_inverter*P_panel
+if P_inverter > P_inverter_max:
+    P_inverter = P_inverter_max     #klopt dit?
 
+P_inverter = P_inverter[:,:,0]                                      #(365,24)
 
-        #all are (365,24,60) ndarrays
+if not Battery_case:
+    P_offtake = P_load - P_inverter
+    P_battery = np.zeros(P_offtake.shape)
 
-        eta_temp = 1 - beta_ref*(T_cell-T_ref)
-        eta_panel = eta_cell * eta_shade * eta_obstruct * eta_temp * eta_degrad
-
-        P_panel = P_sun*eta_panel                                       #Element wise product
-        P_inverter = eta_inverter*P_panel
+else:
+    P_inverter.reshape(365*24)
+    for t in range(P_inverter.shape[0]):
+        if P_inverter[t] > P_inverter_max:
--- effective_irradiance/solar irradiance_v2.py
+++ effective_irradiance/solar irradiance_v2.py
@@ -2,7 +2,8 @@ import numpy as np
 import ephem
 import pandas as pd
 import matplotlib.pyplot as plt
-from datetime import datetime
+from datetime import datetime, timedelta
+import random
 
 import os
 current_dir = os.getcwd()
@@ -17,6 +18,9 @@ date_time = irradiance_data['DateTime']
 global_horizontal_irradiance = irradiance_data['GlobRad'].to_numpy()
 global_diffusive_irradiance = irradiance_data['DiffRad'].to_numpy()
 
+# irradiance_data_old = pd.read_csv(project_dir+'/Data_processing/Input_data/Irradiance_data.csv')
+# date_time_old = irradiance_data_old['DateTime']
+
 # print(len(date_time))
 # print(global_horizontal_irradiance.shape)
 # print(global_diffusive_irradiance.shape)
@@ -25,12 +29,32 @@ global_diffusive_irradiance = irradiance_data['DiffRad'].to_numpy()
 
 """#Helper functions"""
 
+def date_time_conversion(date_time_string):
+
+    datetime_obj = datetime.strptime(date_time_string, "%d/%m/%Y %H:%M")
+    year = datetime_obj.year
+    month = datetime_obj.month
+    day = datetime_obj.day
+    hour = datetime_obj.hour
+    minute = datetime_obj.minute
+
+    dt_input = datetime(year, month, day, hour, minute)
+
+    if dt_input < datetime(2018,3,25,2,0):
+        dt_target = dt_input - timedelta(hours=1)
+    elif dt_input < datetime(2018,10,28,3,0):
+        dt_target = dt_input - timedelta(hours=2)
+    else:
+        dt_target = dt_input - timedelta(hours=1)
+    return dt_target
+
 def get_sun_position(latitude, longitude, date_time):
     observer = ephem.Observer()
     observer.lat = str(latitude)
     observer.lon = str(longitude)
-    # observer.date = date_time
-    observer.date = ephem.date(date_time)
+    # observer.date = date_times
+    # observer.date = ephem.date(date_time)
+    observer.date = date_time_conversion(date_time)
 
     sun = ephem.Sun(observer)
 
@@ -46,35 +70,46 @@ def get_sun_position(latitude, longitude, date_time):
 # Assuming solar_angle_data is loaded from somewhere, and it contains:
 # solar_time_vec, azimuth_degree, zenith_degree
 
-def get_solar_irradiance(irradiance_GHI, irradiance_DHI, south_tilt, east_west_tilt, azimuth_degree, zenith_degree):
+def get_solar_irradiance(irradiance_GHI, irradiance_DHI, south_tilt_degree, east_west_tilt_degree, azimuth_degree, zenith_degree):
     # Convert to radians
     azimuth_rad = np.radians([azimuth_degree])  # Ensure it's an array
     zenith_rad = np.radians([zenith_degree])
 
+    south_tilt_rad = np.radians([south_tilt_degree])  # Ensure it's an array
+    east_west_tilt_rad = np.radians([east_west_tilt_degree])
+
     #the solar position vector calculation
+    # solar_position = np.array([np.sin(zenith_rad) * np.sin(azimuth_rad),
+    #                            np.sin(zenith_rad) * np.cos(azimuth_rad),
+    #                            np.cos(zenith_rad)]).T
     solar_position = np.array([np.sin(zenith_rad) * np.sin(azimuth_rad),
                                np.sin(zenith_rad) * np.cos(azimuth_rad),
-                               np.cos(zenith_rad)]).T
+                               np.cos(zenith_rad)]).reshape((3,))
+    # print(solar_position)
 
     # The position of the solar panels
     azimuth_E = np.pi / 2
     azimuth_S = np.pi
     azimuth_W = 3 * np.pi / 2
 
-    panel_E = np.array([np.sin(east_west_tilt) * np.sin(azimuth_E),
-                        np.sin(east_west_tilt) * np.cos(azimuth_E),
-                        np.cos(east_west_tilt)])
-    panel_S = np.array([np.sin(south_tilt) * np.sin(azimuth_S),
-                        np.sin(south_tilt) * np.cos(azimuth_S),
-                        np.cos(south_tilt)])
-    panel_W = np.array([np.sin(east_west_tilt) * np.sin(azimuth_W),
-                        np.sin(east_west_tilt) * np.cos(azimuth_W),
-                        np.cos(east_west_tilt)])
-
+    panel_E = np.array([np.sin(east_west_tilt_rad) * np.sin(azimuth_E),
+                        np.sin(east_west_tilt_rad) * np.cos(azimuth_E),
+                        np.cos(east_west_tilt_rad)])
+    panel_S = np.array([np.sin(south_tilt_rad) * np.sin(azimuth_S),
+                        np.sin(south_tilt_rad) * np.cos(azimuth_S),
+                        np.cos(south_tilt_rad)])
+    panel_W = np.array([np.sin(east_west_tilt_rad) * np.sin(azimuth_W),
+                        np.sin(east_west_tilt_rad) * np.cos(azimuth_W),
+                        np.cos(east_west_tilt_rad)])
+    # print(panel_E)
     # Calculate the inner product between the solar position vector and the panel orientation vector: gives cosine(alpha)
-    DNI_amplification_E = np.sum(solar_position * panel_E, axis=1)
-    DNI_amplification_S = np.sum(solar_position * panel_S, axis=1)
-    DNI_amplification_W = np.sum(solar_position * panel_W, axis=1)
+    # DNI_amplification_E = np.sum(solar_position * panel_E, axis=1)
+    # DNI_amplification_S = np.sum(solar_position * panel_S, axis=1)
+    # DNI_amplification_W = np.sum(solar_position * panel_W, axis=1)
+
+    DNI_amplification_E = np.dot(solar_position,panel_E)
+    DNI_amplification_S = np.dot(solar_position,panel_S)
+    DNI_amplification_W = np.dot(solar_position,panel_W)
 
     # Irradiation from the back does not result in power
     DNI_amplification_E = np.maximum(DNI_amplification_E, 0)
@@ -90,6 +125,10 @@ def get_solar_irradiance(irradiance_GHI, irradiance_DHI, south_tilt, east_west_t
     Irrad_solarpanelE = irradiance_DHI + DNI * DNI_amplification_E
     Irrad_solarpanelS = irradiance_DHI + DNI * DNI_amplification_S
     Irrad_solarpanelW = irradiance_DHI + DNI * DNI_amplification_W
+    # print(f'DNI is: {DNI}')
+    # print(f'DNI amplification East: {DNI_amplification_E}')
+    # print(f'DNI amplification South: {DNI_amplification_S}')
+    # print(f'DNI amplification West: {DNI_amplification_W}')
 
     # return Irrad_solarpanelE.item(), Irrad_solarpanelS.item(), Irrad_solarpanelW.item()
     return Irrad_solarpanelE, Irrad_solarpanelS, Irrad_solarpanelW
@@ -138,50 +177,101 @@ longitude = 5.53972
 south_tilt_angle = 40
 east_west_tilt_angle = 10
 
+#for one instance
+
+
+one_instance_case = False
+if one_instance_case:
+
+    day = 124
+    # day = 124 - 14
+    granularity = 60
+    j= 15*26
+    i = day*24*60+j*int(60/granularity)
+    # i = day*24*60+j*int(60/granularity) - 7*60 - 6
+
+    exact_moment = date_time[i]
+    # exact_moment = date_time_old[i]
+    print(f'exact moment is: {exact_moment}')
+    zenith_angle, azimuth_angle = get_sun_position(latitude,longitude,exact_moment)
+    print(f'zenith angle is: {zenith_angle}')
+    print(f'azimuth angle is: {azimuth_angle}')
+    GHI = global_horizontal_irradiance[i]
+    print(f'GHI is: {GHI}')
+    DHI = global_diffusive_irradiance[i]
+    print(f'DHI is: {DHI}')
+    total_irradiance_E, total_irradiance_S, total_irradiance_W = get_solar_irradiance(GHI,DHI,south_tilt_angle,east_west_tilt_angle,azimuth_angle,zenith_angle)
+    print(f'Total irradiance is: {total_irradiance_W}')
 
 #for one day
 one_day_case = False
 if one_day_case:
-    granularity = 4
+    granularity = 60
 
-    total_irradiance = np.zeros((3,24*granularity))
-    day = 103
+    # day = 129
 
-    for j in range(24*granularity):
-        i = day*24*60+j*int(60/granularity)
-        exact_moment = date_time[i]
-        zenith_angle, azimuth_angle = get_sun_position(latitude,longitude,exact_moment)
-        GHI = global_horizontal_irradiance[i]
-        DHI = global_diffusive_irradiance[i]
-        total_irradiance_E, total_irradiance_S, total_irradiance_W = get_solar_irradiance(GHI,DHI,south_tilt_angle,east_west_tilt_angle,azimuth_angle,zenith_angle)
-        # total_irradiance.append([exact_moment,total_irradiance_E,total_irradiance_S,total_irradiance_W])
-        # total_irradiance[0,i] = exact_moment
-        total_irradiance[0,j] = total_irradiance_E
-        total_irradiance[1,j] = total_irradiance_S
-        total_irradiance[2,j] = total_irradiance_W
+    for day in random.sample(range(365), 20):
+    # for day in [124]:
+        total_irradiance = np.zeros((3,24*granularity))
+        zenits = np.zeros((24*granularity))
+        azimuths = np.zeros((24*granularity))
 
-    # always the same data? -> no
-    # difference = total_irradiance[0,:]-total_irradiance[1,:]
-    # print(difference)
-    # print(all([i == 0 for i in difference]))
 
-    x=[i*granularity for i in range(24)]
-    l=[]
-    for i in range(24):
-          l.append("%s:00"%i)
-          # l.append(" ")
-    plt.figure(figsize=(8, 6))  # Set the figure size
-    plt.xticks(x,l,fontsize=10, rotation=60)
-    plt.plot(total_irradiance[0,:],label='East', color='blue')
-    plt.plot(total_irradiance[1,:],label='South', color='red')
-    plt.plot(total_irradiance[2,:], label='West', color='orange')
-    plt.title(f'Irradiance profile for different panel orientations on day {day}')
-    plt.xlabel('Time')
-    plt.ylabel('Irradiance [W/m²]')
-    plt.legend()  # Add a legend
-    # plt.grid(True)  # Add a grid
-    plt.tight_layout()  # Fit the plot nicely into the figure
-    plt.show()
+        for j in range(24*granularity):
+            i = day*24*60+j*int(60/granularity)
+            exact_moment = date_time[i]
+            zenith_angle, azimuth_angle = get_sun_position(latitude,longitude,exact_moment)
+            GHI = global_horizontal_irradiance[i]
+            DHI = global_diffusive_irradiance[i]
+            total_irradiance_E, total_irradiance_S, total_irradiance_W = get_solar_irradiance(GHI,DHI,south_tilt_angle,east_west_tilt_angle,azimuth_angle,zenith_angle)
+            # total_irradiance.append([exact_moment,total_irradiance_E,total_irradiance_S,total_irradiance_W])
+            # total_irradiance[0,i] = exact_moment
+            total_irradiance[0,j] = total_irradiance_E
+            total_irradiance[1,j] = total_irradiance_S
+            total_irradiance[2,j] = total_irradiance_W
+            zenits[j] = zenith_angle
+            azimuths[j] = azimuth_angle
+
+        # print(np.argmax(total_irradiance[2,:]))
+
+        x=[i*granularity for i in range(24)]
+        l=[]
+        for i in range(24):
+              l.append("%s:00"%i)
+              # l.append(" ")
+        plt.figure(figsize=(8, 6))  # Set the figure size
+        plt.xticks(x,l,fontsize=10, rotation=60)
+        plt.plot(total_irradiance[0,:],label='East', color='blue')
+        plt.plot(total_irradiance[1,:],label='South', color='red')
+        plt.plot(total_irradiance[2,:], label='West', color='orange')
+        plt.title(f'Irradiance profile for different panel orientations on day {day}')
+        plt.xlabel('Time')
+        plt.ylabel('Irradiance [W/m²]')
+        plt.legend()  # Add a legend
+        # plt.grid(True)  # Add a grid
+        plt.tight_layout()  # Fit the plot nicely into the figure
+        output_file_path = f'Output_data/figures/plot_irradiance_profiles_for_different_panel_orientations_for_one_day_{day}'
+        # plt.savefig(output_file_path)
+        plt.show()
+
+        # x=[i*granularity for i in range(24)]
+        # l=[]
+        # for i in range(24):
+        #       l.append("%s:00"%i)
+        #       # l.append(" ")
+        # plt.figure(figsize=(8, 6))  # Set the figure size
+        # plt.xticks(x,l,fontsize=10, rotation=60)
+        # plt.plot(zenits,label='zeniths', color='blue')
+        # plt.plot(azimuths,label='azimuths', color='red')
+        # # plt.title(f'Irradiance profile for different panel orientations on day {day}')
+        # plt.xlabel('Time')
+        # # plt.ylabel('Irradiance [W/m²]')
+        # plt.legend()  # Add a legend
+        # # plt.grid(True)  # Add a grid
+        # plt.tight_layout()  # Fit the plot nicely into the figure
+        # output_file_path = f'Output_data/figures/plot_zenit_profiles_for_different_panel_orientations_for_one_day_{day}'
+        # # plt.savefig(output_file_path)
+        # # plt.show()
 
 #for all days
 all_days_case = False
@@ -247,27 +337,29 @@ for tilt_angle in range(len(tilt_angles)):
 #kWh/m^2
 total_incoming_energy_for_all_angles = np.sum(total_irradiance_for_all_angles,axis=2)/60
 
-# x=[i*10 for i in range(10)]
-# # l=[10*i for i in x]
-# # l=[]
-# # for i in range(24):
-# #       l.append("%s:00"%i)
-#       # l.append(" ")
-#
-# plt.figure(figsize=(8, 6))  # Set the figure size
-# plt.xticks(x,x,fontsize=10)
-# plt.plot(total_incoming_energy_for_all_angles[:,0],label='East', color='blue')
-# plt.plot(total_incoming_energy_for_all_angles[:,1],label='South', color='red')
-# plt.plot(total_incoming_energy_for_all_angles[:,2], label='West', color='orange')
-# plt.xlim(left=0)
-# plt.ylim(bottom=0)
-# plt.title(f'Yearly solar energy yield in function of varying tilt angles')
-# plt.xlabel('Tilt angle [degrees]')
-# plt.ylabel('Solar incoming energy [kWh/m²]')
-# plt.legend()  # Add a legend
-# # plt.grid(True)  # Add a grid
-# plt.tight_layout()  # Fit the plot nicely into the figure
-# plt.show()
+x=[i*10 for i in range(10)]
+# l=[10*i for i in x]
+# l=[]
+# for i in range(24):
+#       l.append("%s:00"%i)
+      # l.append(" ")
+
+plt.figure(figsize=(8, 6))  # Set the figure size
+plt.xticks(x,x,fontsize=10)
+plt.plot(total_incoming_energy_for_all_angles[:,0],label='East', color='blue')
+plt.plot(total_incoming_energy_for_all_angles[:,1],label='South', color='red')
+plt.plot(total_incoming_energy_for_all_angles[:,2], label='West', color='orange')
+plt.xlim(left=0)
+plt.ylim(bottom=0)
+plt.title(f'Yearly solar energy yield in function of varying tilt angles')
+plt.xlabel('Tilt angle [degrees]')
+plt.ylabel('Solar incoming energy [kWh/m²]')
+plt.legend()  # Add a legend
+# plt.grid(True)  # Add a grid
+plt.tight_layout()  # Fit the plot nicely into the figure
+output_file_path = f'Output_data/figures/yearly_solar_energy_yield_per_angle'
+plt.savefig(output_file_path)
+plt.show()
 
 
 """#Plotting"""
